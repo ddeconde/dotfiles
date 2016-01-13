d77cde94b209676e96b07bc1d577c686fc79aad8
David DeConde <ddeconde@gmail.com>
2016-01-12 16:34:34 -0800

    new version comparison functions

--- bin/AppInstall3.sh
+++ bin/AppInstall3.sh
@@ -20,3 +20,37 @@ installApp () {
     mv "" "${APPLICATIONS_DIR}"
   fi
 }
+
+APPLICATIONS_DIR="/Applications"
+
+if_outdated () {
+  local VERSION_ATTR="CFBundleShortVersionString"
+  local INFO_PATH="${APPLICATIONS_DIR}/${APP_NAME}.app/Contents/Info"
+  local version=$( defaults read "${INFO_PATH}" "${VERSION_ATTR}" )
+  
+}
+
+# Compare with one element of version components
+_ver_cmp_1() {
+  (( $1 == $2 )) && return 0
+  (( $1 > $2 )) && return 1
+  (( $1 < $2 )) && return 2
+  # This should not be happening
+  exit 1
+}
+
+ver_cmp() {
+  local A B i result
+  A=(${1//./ })
+  B=(${2//./ })
+  i=0
+  while (( i < ${#A[@]} )) && (( i < ${#B[@]})); do
+    _ver_cmp_1 "${A[i]}" "${B[i]}"
+    result=$?
+    [[ $result =~ [12] ]] && return $result
+    let i++
+  done
+  # Which has more, then it is the newer version
+  _ver_cmp_1 "${#A[i]}" "${#B[i]}"
+  return $?
+}
