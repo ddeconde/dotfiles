dc6f7bc5077680ad8e865e976074a50a45b8939c
David DeConde <ddeconde@l0x0dr0m3.local>
2016-01-24 22:59:05 -0800

    added app installing functions

--- bin/AppInstall3.sh
+++ bin/AppInstall3.sh
@@ -6,26 +6,48 @@ main () {
   # if is_outdated $1 $2; then
   #   echo "outdated"
   # fi
+
+  install_app "https://iterm2.com/downloads/stable/iTerm2-2_1_4.zip" "iTerm" "zip"
+  install_app "https://s3.amazonaws.com/spectacle/downloads/Spectacle+1.0.1.zip" "Spectacle" "zip"
+  install_app "https://github.com/macvim-dev/macvim/releases/download/snapshot-94/MacVim.dmg" "MacVim" "dmg"
+  install_app "http://london.kapeli.com/Dash.zip" "Dash" "zip"
+  install_app "http://bombich.com/software/download_ccc.php?v=latest" "Carbon Copy Cloner" "zip"
+  install_app "http://www.sparklabs.com/downloads/Viscosity.dmg" "Viscosity" "dmg"
+  install_app "http://unarchiver.c3.cx/downloads/TheUnarchiver3.10.1.dmg" "The Unarchiver" "dmg"
+  install_app "http://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US" "Firefox" "dmg"
+  install_app "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac4/1Password-6.0.1.zip" "1Password 6" "zip"
+  install_app "http://tug.org/cgi-bin/mactex-download/MacTeX.pkg" "MacTeX" "pkg"
+  install_app "https://www.vmware.com/go/try-fusion-en" "VMware Fusion" "dmg"
+  install_app "https://www.obdev.at/downloads/littlesnitch/LittleSnitch-3.6.1.dmg" "Little Snitch Installer" "dmg"
+  install_app "https://www.obdev.at/downloads/MicroSnitch/MicroSnitch-1.2.zip" "Micro Snitch" "zip"
+  install_app "https://www.obdev.at/downloads/launchbar/LaunchBar-6.5.dmg" "LaunchBar" "dmg"
+  install_app "" "TorBrowser" "dmg"
+  install_app "http://download.transmissionbt.com/files/Transmission-2.84.dmg" "Transmission" "dmg"
+  install_app "https://adiumx.cachefly.net/Adium_1.5.10.dmg" "Adium" "dmg"
+  install_app "http://get.videolan.org/vlc/2.2.1/macosx/vlc-2.2.1.dmg" "VLC" "dmg"
+  install_app "https://update.cyberduck.io/Cyberduck-4.7.3.zip" "Cyberduck" "zip"
 }
 
-get_app () {
-  curl -s -L -o ${APP_PATH} ${APP_URL}
-  if is_not_installed ${APP_NAME}; then
-    install_app "${APP_PATH}" "${TYPE}"
-  elif is_outdated "${APP_NAME}"; then
-    # perhaps remove current app first?
-    install_app "${APP_PATH}" "${TYPE}"
-  fi
-}
+APP_DIR="/Applications"
 
-install_app () {
-  local APP_NAME=$1
-  local APP_PATH=$2
+get_application () {
+  local APP_URL=$1
+  local APP_NAME=$2
   local FILE_TYPE=$3
+  local APP_PATH="${TMP_DIR}/${APP_NAME}.${FILE_TYPE}"
+  curl -s -L -o ${APP_PATH} ${APP_URL}
+}
+
+install_application () {
+  local APP_NAME=$1
+  local FILE_TYPE=$2
+  local APP_PATH="${TMP_DIR}/${APP_NAME}.${FILE_TYPE}"
   local MOUNT_PT="/Volumes/${APP_NAME}"
 
-  # Download application
-  curl -s -L -o ${APP_PATH} ${APP_URL}
+  # Skip over already installed applications; no updating
+  if is_installed ${APP_NAME}; then
+    return 0
+  fi
 
   # Install according to type
   case ${FILE_TYPE} in
@@ -33,18 +55,15 @@ install_app () {
       # yes handles required interactive agreements
       yes | hdiutil attach ${APP_PATH} -nobrowse -mountpoint ${MOUNT_PT} > /dev/null 2>&1
       cp -R "${MOUNT_PT}/${APP_NAME}.app" "${APP_DIR}"
-      hdiutil detach ${MOUNT_PT}
-      rm -rf ${APP_PATH}
+      hdiutil detach ${MOUNT_PT} > /dev/null 2>&1
     ;;
     "zip")
       unzip -qq ${APP_PATH}
       mv "${APP_NAME}.app" "${APP_DIR}"
-      rm -rf ${APP_PATH}
     ;;
     "tar")
       tar -zxf ${APP_PATH}
       mv "${APP_NAME}.app" "${APP_DIR}"
-      rm -rf ${APP_PATH}
     ;;
     "pkg")
       sudo installer -pkg ${APP_PATH} -target /
@@ -52,8 +71,19 @@ install_app () {
   esac
 }
 
-APP_DIR="/Applications"
+install_app () {
+  local APP_URL=$1
+  local APP_NAME=$2
+  local FILE_TYPE=$3
+  local APP_PATH="${TMP_DIR}/${APP_NAME}.${FILE_TYPE}"
 
+  get_application ${APP_URL} ${APP_NAME} ${FILE_TYPE}
+  install_application ${APP_NAME} ${FILE_TYPE}
+}
+
+clean_up_apps () {
+  rm -rf ${TMP_DIR}
+}
 
 _compare_version_component() {
   # Compare a single component of two version numbers
@@ -92,6 +122,13 @@ is_outdated_compared_to () {
     return 0
   fi
     return 1
+}
+
+is_installed () {
+  if [[ -d "${APP_DIR}/${1}.app" ]]; then
+    return 0
+  fi
+  return 1
 }
 
 is_not_installed () {
